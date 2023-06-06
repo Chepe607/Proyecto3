b36cfc2d49987fd72a924e35cd28ae19fb718abf
Chepe607 <135440386+Chepe607@users.noreply.github.com>
2023-06-06 15:34:14 -0600

    Cambios definitivos del esqueleto y la funcionalidad de agregar citas

--- Programa3Contenido/programa3.py
+++ Programa3Contenido/programa3.py
@@ -74,34 +74,53 @@ def programar_citas ():
             return False
     
     def validar_entries (): #Restricciones de los datos
-        if len (numero_placa) > 8:
+        if var_primera_vez.get () == False and var_reinspeccion.get () == False:
+            MessageBox.showerror ("Error", "Debe seleccionar al menos un tipo de cita")
+            return False
+
+        if len (numero_placa.get ()) > 8:
             MessageBox.showerror ("Error", "El número de placa deber estar entre 1 y 8 caracteres")
-        if len (numero_placa) == 0:
-            MessageBox.showerror ("Error", "Debe ingresar un número de placa")
-
-        if len (marca_vehiculo) > 15 or len (marca_vehiculo) < 3:
-            MessageBox.showerror ("Error", "La marca del vehículo debe ser mayor o igual a 3 caracteres y menor o igual a 15 caracteres")
-        if len (marca_vehiculo) == 0:
-            MessageBox.showerror ("Error", "Debe ingresar una marca de vehículo")
+            return False
             
-        if len (modelo) > 15:
+        if len (numero_placa. get()) == 0:
+            MessageBox.showerror ("Error", "Debe ingresar un número de placa")
+            return False
+        
+        if len (marca_vehiculo.get ()) > 15 or len (marca_vehiculo.get ()) < 3:
+            MessageBox.showerror ("Error", "La marca del vehículo debe ser mayor o igual a 3 caracteres y menor o igual a 15 caracteres")
+            return False
+        
+        if len (marca_vehiculo.get ()) == 0:
+            MessageBox.showerror ("Error", "Debe ingresar una marca de vehículo")
+            return False
+            
+        if len (modelo.get ()) > 15:
             MessageBox.showerror ("Error", "El modelo a ingresar debe tener menos de 15 caracteres")
+            return False
 
-        if len (modelo) < 1:
+        if len (modelo.get ()) < 1:
             MessageBox.showerror ("Error", "Debe ingresar un modelo del medio de transporte")
+            return False
 
-        if len (propetario) > 40 or len (propetario) < 6:
+        if len (propetario.get ()) > 40 or len (propetario.get ()) < 6:
             MessageBox.showerror ("Error", "El dato a ingresar del propetario debe tener una extensión entre 6 y 40 caracteres")
+            return False
 
-        if len (direccion_fisica) < 10 or len (direccion_fisica) > 40:
+        if len (direccion_fisica.get ()) < 10 or len (direccion_fisica.get ()) > 40:
             MessageBox.showerror ("Error", "La dirección física debe ser una entre 10 y 40 caracteres")
-
+            return False
+        
+        if var_automatico.get () == False and var_manual.get () == False:
+            MessageBox.showerror ("Error", "Debe seleccionar al menos un tipo de cita")
+            return False
+        
+        return True
     def generar_horas ():
         global cantidad_de_horas_mostrar
         hora_comienzo = 8
         hora_termino = 20
         duracion_citas = 20
-        meses_a_considerar = 12
+        meses_a_considerar = 3
         hora_actual = datetime.now ()
 
         #Lista de las horas a mostrar:
@@ -120,36 +139,60 @@ def programar_citas ():
                 
             hora_actual_mod += timedelta(minutes=duracion_citas)
             
-            
-        print (cantidad_de_horas_mostrar)
 
     def mostrar_manual ():
         valor_actual_manual = var_manual.get ()
+        hora_seleccionada = tk.StringVar ()
 
+
+        def validar_hora_manual (hora_v): #Validación de las horas al seleccionar de manera manual
+            try:
+                datetime.strptime (hora_v, "%I:%M %p")
+                return True
+            except ValueError:
+                return False
+            
+
+        def boton_confirmacion (): #Acción de guardar los datos de manera manual
+            fecha_seleccionada = calendario_fecha.get_date ()
+            hora_seleccionada = hora_entry.get ()
+
+            validacion = validar_hora_manual (hora_seleccionada)
+            if validacion == True:
+                print ("Pasa #1", fecha_seleccionada, hora_seleccionada)
+                return True, fecha_seleccionada, hora_seleccionada
+            else:
+                MessageBox.showerror ("Error", "Hora indicada es inválida")
+                return False, -1, -1
+            
         if valor_actual_manual == True:
             automatico.config (state = "disable")
             ventana_manual = tk.Toplevel ()
             ventana_manual.geometry ("700x550")
+            ventana_manual.title ("Selección manual de fecha y hora")
+
+
             manual_label = tk.Label (ventana_manual, text = "Asignación de fecha y hora", font = "Helvetica 15 bold")
             manual_label.place (x = 200, y = 30)
+            manual_label_instruccion = tk.Label (ventana_manual, text = "Seleccione la fecha y hora deseada para su cita; utilice el botón de abajo para confirmar la selección.", font = "Helvetica 10")
+            manual_label_instruccion.place (x = 58, y = 70)
             fecha_label = tk.Label (ventana_manual, text= "Seleccione la fecha deseada:", font = "Helvetica 13 bold")
             fecha_label.place (x = 58, y = 150)
 
             fecha_actual = datetime.now ()
 
-            calendario_fecha = Calendar(ventana_manual, year = fecha_actual.year, month = fecha_actual.month, day = fecha_actual.day , mindate = fecha_actual)
+            calendario_fecha = Calendar(ventana_manual, year = fecha_actual.year, month = fecha_actual.month, day = fecha_actual.day , mindate = fecha_actual, date_pattern = "dd/mm/yyyy")
             calendario_fecha.place (x = 50, y = 200)
 
             hora_label = tk.Label (ventana_manual, text = "Seleccione la hora deseada:", font = "Helvetica 13 bold")
             hora_label.place (x = 390, y = 150)
-            hora_entry = tk.Entry (ventana_manual, font = "Helvetica 12", width = 25, justify = "center")
+            hora_entry = tk.Entry (ventana_manual, textvariable = hora_seleccionada, font = "Helvetica 12", width = 25, justify = "center")
             hora_entry.place (x= 390, y = 200 )
 
-            boton_confirmacion_manual = tk.Button (ventana_manual, text = "Confirmar cita", font = "Helvetica 12 bold", width = 15, height = 3, bg= "#C7D0D7" )
-            boton_confirmacion_manual.place (x = 250, y = 420) ###FALTA VALIDACION DE LAS HORAS
+            boton_confirmacion_manual = tk.Button (ventana_manual, text = "Confirmar cita", font = "Helvetica 12 bold", width = 15, height = 3, bg= "#08f26e", command = lambda: boton_confirmacion ())
+            boton_confirmacion_manual.place (x = 250, y = 420)
 
-
-
+            
 
         else:
             automatico.config (state = "normal")
@@ -158,25 +201,63 @@ def programar_citas ():
     def mostrar_automatico ():
         valor_actual_automatico = var_automatico.get ()
 
+        def guardar_fecha_automatico (): #Obtener el dato seleccionado del listbox
+            indice_valor_seleccionado = fecha_listbox.curselection() [0]
+            valor_seleccionado = fecha_listbox.get (indice_valor_seleccionado)
+            print (valor_seleccionado)
+
         if valor_actual_automatico == True:
             manual.config (state = "disable")
             generar_horas () #Se generan las horas
 
             ventana_automatico = tk.Toplevel () #Ventana a mostrar
+            ventana_automatico.title ("Selección automática de fecha y hora")
             ventana_automatico.geometry ("300x300")
 
-            lista_horas_listbox = tk.Variable (value = cantidad_de_horas_mostrar) #Lista de las citas
-            fecha_listbox = tk.Listbox (ventana_automatico, listvariable= lista_horas_listbox, height = 10)
+            lista_horas_listbox = tk.Variable (value = cantidad_de_horas_mostrar) #Lista de las citas 
+            fecha_listbox = tk.Listbox (ventana_automatico, listvariable= lista_horas_listbox, height = 10,borderwidth = 2, relief = "sunken") #Generar el listbox
             fecha_listbox.place (x= 80, y = 80 )
             scrollbar_listbox = ttk.Scrollbar(ventana_automatico, orient = "vertical", command = fecha_listbox.yview)
             
-            scrollbar_listbox.place (x= 200, y = 80, height = 160)
+            scrollbar_listbox.place (x= 200, y = 80, height = 160) #Generar el scrollbar
             fecha_listbox["yscrollcommand"] = scrollbar_listbox.set
+
+            #Elemenos extra:
+            automatico_label = tk.Label (ventana_automatico, text = "Asignación automática fecha/hora", font = "Helvetica 12 bold")
+            automatico_label.place (x= 20, y = 30)
+            automatico_instruccion = tk.Label (ventana_automatico, text = "Seleccione de la lista la fecha y hora de la cita", font = "Helvetica 10")
+            automatico_instruccion.place (x = 15, y = 55)
+
+            boton_confirmacion = tk.Button (ventana_automatico, text = "Confirmar cita", font = "Helvetica 10 bold", bg = "#08f26e", command = lambda: guardar_fecha_automatico ())
+            boton_confirmacion.place (x = 90, y = 260)
 
         else:
             manual.config (state = "normal")
 
+    def bloquear_reinspeccion ():
+        valor_actual_primera_vez = var_primera_vez.get ()
 
+        if valor_actual_primera_vez == True:
+            tipo_cita_reinspeccion.config (state = "disable")
+        
+        else:
+            tipo_cita_reinspeccion.config (state = "normal")
+
+    def bloquear_primera_vez ():
+        valor_actual_reinspeccion = var_reinspeccion.get ()
+
+        if valor_actual_reinspeccion == True:
+            tipo_cita_primera_vez.config (state = "disable")
+        else:
+            tipo_cita_primera_vez.config (state = "normal")
+
+    def guardar_cita ():
+        validacion = validar_entries ()
+        if validacion == True:
+            print ("Prueba #1 PASA")
+        else:
+            print ("No pasa")
+    
 
 
 
@@ -191,16 +272,14 @@ def programar_citas ():
     titulo_programar_citas.grid (row = 0, column = 0, padx = 300, pady = 40)
     instrucciones_programar_citas = tk.Label (ventana_programar_citas, text = "Para agregar una cita, agregue la información solicitada.", font = "Helvetica 13")
     instrucciones_programar_citas.place (x = 220, y = 90)
-    boton = tk.Button (ventana_programar_citas, command = lambda: generar_horas ())
-    boton.place (x= 500, y = 0)
 
 
     #Tipo de cita elementos
     var_primera_vez = tk.BooleanVar ()
     var_reinspeccion = tk.BooleanVar ()
     tipo_cita_label = tk.Label (ventana_programar_citas, text = "Tipo de cita:", font = "Helvetica 14 bold")
-    tipo_cita_primera_vez = tk.Checkbutton (ventana_programar_citas, text = "Primera vez", variable = var_primera_vez)
-    tipo_cita_reinspeccion = tk.Checkbutton (ventana_programar_citas, text = "Reinspección", variable = var_reinspeccion)
+    tipo_cita_primera_vez = tk.Checkbutton (ventana_programar_citas, text = "Primera vez", variable = var_primera_vez, command = lambda : bloquear_reinspeccion ())
+    tipo_cita_reinspeccion = tk.Checkbutton (ventana_programar_citas, text = "Reinspección", variable = var_reinspeccion, command = lambda : bloquear_primera_vez ())
     tipo_cita_label.place (x = 220, y = 160)
     tipo_cita_primera_vez.place (x = 220, y = 200)
     tipo_cita_reinspeccion.place (x = 325, y = 200)
@@ -266,6 +345,13 @@ def programar_citas ():
     automatico = tk.Checkbutton (ventana_programar_citas, text = "Automático", variable = var_automatico, command = lambda : mostrar_automatico ())
     manual.place (x= 450, y =600)
     automatico.place (x= 520, y = 600)
+
+    #Agendar cita o cancelar elementos:
+    boton_guardar = tk.Button (ventana_programar_citas, text = "Agendar la cita", font = "Helvetica 10 bold", bg = "#08f26e", width =  23, height = 3, command = lambda: guardar_cita ())
+    boton_guardar.place (x = 220, y = 650)
+
+    boton_cancelar = tk.Button (ventana_programar_citas, text = "Cancelar asignación de cita", font = "Helvetica 10 bold", bg = "#ff3333", height = 3, command = lambda: ventana_programar_citas.destroy () )
+    boton_cancelar.place (x = 450, y = 650)
 
     
 
