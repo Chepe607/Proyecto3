f8aeb03be5357f732e11e212aa9c6457badc47c5
Emmanuel Rodríguez Rivas <emmanueldrr22@gmail.com>
2023-06-06 23:03:32 -0600

    Acomodé las funciones arriba y eliminé el import validate_email que hice porque no se utilizará.

--- Programa3Contenido/programa3.py
+++ Programa3Contenido/programa3.py
@@ -9,57 +9,9 @@ from tkinter import *
 from tkinter import messagebox as MessageBox
 from datetime import datetime, timedelta
 from email_validator import validate_email, EmailNotValidError
-import validate_email
 from tkcalendar import *
 
-
-#Programa principal (ventana principal)
-ventana_principal = tk.Tk ()
-ventana_principal.title ("Menú principal ReTeVe")
-ventana_principal.geometry ("540x480")
-ventana_principal.config (bg= "white")
-
-
-
-#Menú de barra de la ventana principal
-menuBarra = Menu(ventana_principal)
-menuBarra.add_command (label = "Lista de Fallas", command = lambda: lista_de_fallas ())
-menuBarra.add_command (label = "Configuración del sistema", command = lambda: configuracion_sistema ())
-menuBarra.add_command (label = "Ayuda", command = lambda: ayuda ())
-menuBarra.add_command (label = "Acerca de", command= lambda: acerca_de ())
-menuBarra.add_command (label= "Salir", command = lambda: salir ())
-ventana_principal.config (menu = menuBarra)
-
-#Elementos ventana principal
-bienvenida_menu = tk.Label (ventana_principal, text = "Menú ReTeVe: Revisión Técnica de Vehículos", font = "Helvetica 15 bold", bg = "white")
-bienvenida_menu.place (x= 12, y = 50)
-bienvenida = Label (ventana_principal, text = "Bienvenido al programa de revisión de vehiculos ReTeVe", font = "Helvetica 9 bold", bg = "white")
-bienvenida.place (x= 12, y = 100)
-navegacion = tk.Label (ventana_principal, text = "Para continuar, por favor utilice las opciones de la parte superior o inferior de la ventana.", font = "Helvetica 9 bold", bg = "white")
-navegacion.place (x= 12, y = 150)
-
-#Botones de opciones principales
-boton_programar_cita = tk.Button (ventana_principal, text = "Programar cita", font = "Helvetica 10 bold", width = 20, height = 3, bg = "#C7D0D7", command = lambda: programar_citas())
-boton_programar_cita.place (x= 70, y = 230)
-boton_cancelar_cita = tk.Button (ventana_principal, text = "Cancelar cita",font = "Helvetica 10 bold",  width = 20, height = 3, bg = "#C7D0D7", command = lambda: cancelar_citas () )
-boton_cancelar_cita.place (x= 70, y = 330)
-boton_ingresar_cita = tk.Button (ventana_principal, text = "Ingresar cita",font = "Helvetica 10 bold",  width = 20, height = 3, bg = "#C7D0D7", command = lambda: ingresar_citas () )
-boton_ingresar_cita.place (x= 300, y = 230)
-boton_tablero = tk.Button (ventana_principal, text = "Tablero revisión",font = "Helvetica 10 bold", width = 20, height = 3, bg = "#C7D0D7", command = lambda: tablero_revision () )
-boton_tablero.place (x= 300, y = 330)
-
-#Funciones del programa
-contador_citas = 1
-cantidad_de_horas_mostrar = []
-numero_placa = tk.StringVar ()
-marca_vehiculo = tk.StringVar ()
-modelo = tk.StringVar ()
-propetario = tk.StringVar ()
-telefono = tk.StringVar ()
-correo = tk.StringVar ()
-direccion_fisica = tk.StringVar ()
-
-lista_vehiculos = ["Automóvil particular y vehículo de carga liviana (<3500kg)", "Automóvil particular y vehículo de carga liviana (3500kg - 8000kg)", "Vehículo de carga pesada y cabezales (8000kg -)", "Taxis", "Busetas", "Motocicletas", "Equipo especial de obras", "Equipo especial de agrícola"]
+#Funciones
 def programar_citas ():
     global cantidad_de_horas_mostrar
 
@@ -359,6 +311,60 @@ def acerca_de():
     MessageBox.showinfo("Acerca de", "Programa ReTeVe\nVersión del programa 1.0\
 \nFecha de creación: 19/06/2023\nDesarrollado por: Emmanuel Rodríguez Rivas y Jose Miguel González Barrantes")
     
+
+
+#---------------------------Programa principal (ventana principal) ----------------------------
+ventana_principal = tk.Tk ()
+ventana_principal.title ("Menú principal ReTeVe")
+ventana_principal.geometry ("540x480")
+ventana_principal.config (bg= "white")
+
+
+
+#Menú de barra de la ventana principal
+menuBarra = Menu(ventana_principal)
+menuBarra.add_command (label = "Lista de Fallas", command = lambda: lista_de_fallas ())
+menuBarra.add_command (label = "Configuración del sistema", command = lambda: configuracion_sistema ())
+menuBarra.add_command (label = "Ayuda", command = lambda: ayuda ())
+menuBarra.add_command (label = "Acerca de", command= lambda: acerca_de ())
+menuBarra.add_command (label= "Salir", command = lambda: salir ())
+ventana_principal.config (menu = menuBarra)
+
+#Elementos ventana principal
+bienvenida_menu = tk.Label (ventana_principal, text = "Menú ReTeVe: Revisión Técnica de Vehículos", font = "Helvetica 15 bold", bg = "white")
+bienvenida_menu.place (x= 12, y = 50)
+bienvenida = Label (ventana_principal, text = "Bienvenido al programa de revisión de vehiculos ReTeVe", font = "Helvetica 9 bold", bg = "white")
+bienvenida.place (x= 12, y = 100)
+navegacion = tk.Label (ventana_principal, text = "Para continuar, por favor utilice las opciones de la parte superior o inferior de la ventana.", font = "Helvetica 9 bold", bg = "white")
+navegacion.place (x= 12, y = 150)
+
+#Botones de opciones principales
+boton_programar_cita = tk.Button (ventana_principal, text = "Programar cita", font = "Helvetica 10 bold", width = 20, height = 3, bg = "#C7D0D7", command = lambda: programar_citas())
+boton_programar_cita.place (x= 70, y = 230)
+boton_cancelar_cita = tk.Button (ventana_principal, text = "Cancelar cita",font = "Helvetica 10 bold",  width = 20, height = 3, bg = "#C7D0D7", command = lambda: cancelar_citas () )
+boton_cancelar_cita.place (x= 70, y = 330)
+boton_ingresar_cita = tk.Button (ventana_principal, text = "Ingresar cita",font = "Helvetica 10 bold",  width = 20, height = 3, bg = "#C7D0D7", command = lambda: ingresar_citas () )
+boton_ingresar_cita.place (x= 300, y = 230)
+boton_tablero = tk.Button (ventana_principal, text = "Tablero revisión",font = "Helvetica 10 bold", width = 20, height = 3, bg = "#C7D0D7", command = lambda: tablero_revision () )
+boton_tablero.place (x= 300, y = 330)
+
+#Funciones del programa
+contador_citas = 1
+cantidad_de_horas_mostrar = []
+numero_placa = tk.StringVar ()
+marca_vehiculo = tk.StringVar ()
+modelo = tk.StringVar ()
+propetario = tk.StringVar ()
+telefono = tk.StringVar ()
+correo = tk.StringVar ()
+direccion_fisica = tk.StringVar ()
+
+#Variables
+
+
+
+lista_vehiculos = ["Automóvil particular y vehículo de carga liviana (<3500kg)", "Automóvil particular y vehículo de carga liviana (3500kg - 8000kg)", "Vehículo de carga pesada y cabezales (8000kg -)", "Taxis", "Busetas", "Motocicletas", "Equipo especial de obras", "Equipo especial de agrícola"]
+
 
 ventana_principal.mainloop ()
 
